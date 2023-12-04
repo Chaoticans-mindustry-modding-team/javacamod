15ff456aa91b29487a38392a7f519cbe423ec643
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2023-12-04 13:44:08 -0700

    Update JavacamodBlocks.java

--- src/javacamod/content/JavacamodBlocks.java
+++ src/javacamod/content/JavacamodBlocks.java
@@ -4,6 +4,7 @@ import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.struct.*;
 import arc.util.*;
+import javacamod.JavacamodItems;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.entities.effect.*;
@@ -29,28 +30,48 @@ import static mindustry.type.ItemStack.*;
 @SuppressWarnings("deprecation")
 public class JavacamodBlocks{
     //walls
-    public static Block redWall, largeRedWall, colossalRedWall 
+    public static Block redWall, largeRedWall, colossalRedWall, blueWall, largeBlueWall, colossalBlueWall 
 
 
     ;
     
     public static void load() {
         redWall = new Wall("red-wall"){{
-            requirements(Category.defense, with(Items.copper, 6));
+            requirements(Category.defense, with(JavacamodItems.red, 6));
             health = 80;
             envDisabled |= Env.scorching;
         }};
     
      
         largeRedWall = new Wall("large-red-wall"){{
-            requirements(Category.defense, with(Items.copper, 24));
+            requirements(Category.defense, with(JavacamodItems.red, 24));
             health = 80;
             size = 2;
             envDisabled |= Env.scorching;
         }};
 
         colossalRedWall = new Wall("colossal-red-wall"){{
-            requirements(Category.defense, with(Items.copper, 54));
+            requirements(Category.defense, with(JavacamodItems.red, 54));
+            health = 80;
+            size = 3;
+            envDisabled |= Env.scorching;
+        }};
+
+        blueWall = new Wall("blue-wall"){{
+            requirements(Category.defense, with(JavacamodItems.blue, 6));
+            health = 80;
+            envDisabled |= Env.scorching;
+        }};
+
+        largeBlueWall = new Wall("large-blue-wall"){{
+            requirements(Category.defense, with(JavacamodItems.blue, 24));
+            health = 80;
+            size = 2;
+            envDisabled |= Env.scorching;
+        }};
+
+        colossalBlueWall = new Wall("colossal-blue-wall"){{
+            requirements(Category.defense, with(JavacamodItems.blue, 54));
             health = 80;
             size = 3;
             envDisabled |= Env.scorching;
