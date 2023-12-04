bf35b4cb6e8f20fe37d7a141beb8f9f336950cf5
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2023-12-03 19:45:24 -0700

    Create JavacamodItems.java

---
+++ src/javacamod/content/JavacamodItems.java
@@ -0,0 +1,245 @@
+package javacamod.content;
+
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class JavacamodItems{
+    public static Item
+    red, darkred, crimson, green, darkgreen, poison, blue, darkblue, navy, black, white, lightgray, gray, darkgray, 
+    spectra1, spectra2, spectra3, clear, yellow, cyan, pink, orange, lime, emerald, sapphire, purple, hotpink, cortexian, 
+    crab, urog, canblack, canred, canorange, canyellow, canblue, canteal, cangreen, canwhite; 
+
+
+    public static void load(){
+      red = new Item("Red", Color.valueOf("ff0000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      darkred = new Item("Dark Red", Color.valueOf("770000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      crimson = new Item("Crimson", Color.valueOf("300000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+      
+      green = new Item("Green", Color.valueOf("00ff00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      darkgreen = new Item("Dark Green", Color.valueOf("007700")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      poison = new Item("Poison", Color.valueOf("003000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      blue = new Item("Blue", Color.valueOf("0000ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      darkblue = new Item("Dark Blue", Color.valueOf("000077")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      navy = new Item("Navy", Color.valueOf("000030")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      black = new Item("Black", Color.valueOf("000000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      white = new Item("White", Color.valueOf("ffffff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      lightgray = new Item("Lightgray", Color.valueOf("bbbbbb")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      gray = new Item("Gray", Color.valueOf("777777")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      darkgray = new Item("Darkgray", Color.valueOf("303030")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      spectra1 = new Item("Spectra One", Color.valueOf("aaaaaabb")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      spectra2 = new Item("Spectra Two", Color.valueOf("aaaaaa88")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      spectra3 = new Item("Spectra Three", Color.valueOf("aaaaaa44")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      clear = new Item("Clear", Color.valueOf("aaaaaa00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      yellow = new Item("Yellow", Color.valueOf("ffff00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      cyan = new Item("Cyan", Color.valueOf("00ffff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      pink = new Item("Pink", Color.valueOf("ff00ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      orange = new Item("Orange", Color.valueOf("ff6600")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      lime = new Item("Lime", Color.valueOf("66ff00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      emerald = new Item("Emerald", Color.valueOf("00ff66")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      sapphire = new Item("Sapphire", Color.valueOf("0066ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      purple = new Item("Purple", Color.valueOf("6600ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      hotpink = new Item("Hot Pink", Color.valueOf("ff0066")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      cortexian = new Item("Cortexian", Color.valueOf("0050ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      crab = new Item("Crab", Color.valueOf("ff0050")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      urog = new Item("Urog", Color.valueOf("4400ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      canblack = new Item("Canvas Black", Color.valueOf("362944")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      canred = new Item("Canvas Red", Color.valueOf("c45d9f")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      canorange = new Item("Canvas Orange", Color.valueOf("e39aac")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      canyellow = new Item("Canvas Yellow", Color.valueOf("f0dab1")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      canblue = new Item("Canvas Blue", Color.valueOf("6461c2")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      canteal = new Item("Canvas Teal", Color.valueOf("2ba9b4")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      cangreen = new Item("Canvas Green", Color.valueOf("93d4b5")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      canwhite = new Item("Canvas White", Color.valueOf("f0f6e8")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+   }
+      
+}
