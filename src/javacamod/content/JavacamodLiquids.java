2f3a21f0c74231dc9393bd0749e068d140b46687
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-01-31 17:58:22 -0700

    Update JavacamodLiquids.java

--- src/javacamod/content/JavacamodLiquids.java
+++ src/javacamod/content/JavacamodLiquids.java
@@ -1,1 +1,243 @@
+package javacamod.content;
 
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class JavacamodItems{
+    public static Item
+    liquidRed, liquidDarkred, blood, liquidGreen, liquidDarkgreen, liquidPoison, liquidBlue, liquidDarkblue, liquidNavy, liquidBlack, liquidWhite, liquidLightgray, liquidGray, liquidDarkgray, ectoplasm1, ectoplasm2, ectoplasm3, liquidClear, liquidYellow, liquidCyan, liquidPink, liquidOrange, liquidLime, liquidEmerald, liquidSapphire, liquidPurple, liquidHotpink, liquidFemboy, liquidCrab, liquidUrog, liquidCanblack, liquidCanred, liquidCanorange, liquidCanyellow, liquidCanblue, liquidCanteal, liquidCangreen, liquidCanwhite; 
+
+
+    public static void load(){
+      liquidRed = new Liquid("liquid-red", Color.valueOf("ff0000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidDarkred = new Liquid("liquid-darkred", Color.valueOf("770000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      blood = new Liquid("blood", Color.valueOf("300000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+      
+      liquidGreen = new Liquid("liquid-green", Color.valueOf("00ff00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      darkgreen = new Liquid("liquid-darkgreen", Color.valueOf("007700")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidPoison = new Liquid("liquid-poison", Color.valueOf("003000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidBlue = new Liquid("liquid-blue", Color.valueOf("0000ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidDarkblue = new Liquid("liquid-darkblue", Color.valueOf("000077")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidNavy = new Liquid("liquid-navy", Color.valueOf("000030")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidBlack = new Liquid("liquid-black", Color.valueOf("000000")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidWhite = new Liquid("liquid-white", Color.valueOf("ffffff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidLightgray = new Liquid("liquid-lightgray", Color.valueOf("bbbbbb")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidGray = new Liquid("liquid-gray", Color.valueOf("777777")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidDarkgray = new Liquid("liquid-darkgray", Color.valueOf("303030")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      ectoplasm1 = new Liquid("ectoplasm-one", Color.valueOf("aaaaaabb")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      ectoplasm2 = new Liquid("ectoplasm-two", Color.valueOf("aaaaaa88")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      ectoplasm3 = new Liquid("ectoplasm-three", Color.valueOf("aaaaaa44")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidClear = new Liquid("liquid-clear", Color.valueOf("aaaaaa00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidYellow = new Liquid("liquid-yellow", Color.valueOf("ffff00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCyan = new Liquid("liquid-cyan", Color.valueOf("00ffff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidPink = new Liquid("liquid-pink", Color.valueOf("ff00ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidOrange = new Liquid("liquid-orange", Color.valueOf("ff6600")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidLime = new Liquid("liquid-lime", Color.valueOf("66ff00")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidEmerald = new Liquid("liquid-emerald", Color.valueOf("00ff66")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidSapphire = new Liquid("liquid-sapphire", Color.valueOf("0066ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      venom = new Liquid("venom", Color.valueOf("6600ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidHotpink = new Liquid("liquid-hot-pink", Color.valueOf("ff0066")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidFemboy = new Liquid("liquid-femboy", Color.valueOf("0050ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCrab = new Liquid("liquid-crab", Color.valueOf("ff0050")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidUrog = new Liquid("liquid-urog", Color.valueOf("4400ff")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCanblack = new Liquid("liquid-canvas-black", Color.valueOf("362944")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCanred = new Liquid("liquid-canvas-red", Color.valueOf("c45d9f")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCanorange = new Liquid("liquid-canvas-orange", Color.valueOf("e39aac")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCanyellow = new Liquid("liquid-canvas-yellow", Color.valueOf("f0dab1")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCanblue = new Liquid("liquid-canvas-blue", Color.valueOf("6461c2")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCanteal = new Liquid("liquid-canvas-teal", Color.valueOf("2ba9b4")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCangreen = new Liquid("liquid-canvas-green", Color.valueOf("93d4b5")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+      liquidCanwhite = new Liquid("liquid-canvas-white", Color.valueOf("f0f6e8")){{
+          hardness = 1;
+          cost = 0.5f;
+          alwaysUnlocked = true;
+      }};
+
+   }
+      
+}
