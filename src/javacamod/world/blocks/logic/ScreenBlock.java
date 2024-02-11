403cbff6a25bfe1a15e5412000b63429ea207317
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-02-11 10:19:59 -0700

    Update ScreenBlock.java

--- src/javacamod/world/blocks/logic/ScreenBlock.java
+++ src/javacamod/world/blocks/logic/ScreenBlock.java
@@ -29,17 +29,17 @@ public class ScreenBlock extends Block{
 
     public void load(){
         side1=Core.atlas.load(name+"-side1");
-}
+};
     public void load(){
         side2=Core.atlas.load(name+"-side2");
-}
+};
 
     public void load(){
         corner1=Core.atlas.load(name+"-corner1");
-}
+};
     public void load(){
         corner2=Core.atlas.load(name+"-corner2");
-}
+};
 
     protected @Nullable Pixmap previewPixmap;
     protected @Nullable Texture previewTexture;
