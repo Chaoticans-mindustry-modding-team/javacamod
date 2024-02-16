79482a84d4245c571e7255353c2dd6314c52850a
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2024-02-16 15:37:44 +0100

    A lot of files

--- src/javacamod/world/blocks/power/HexBlock.java
+++ src/javacamod/world/blocks/power/HexBlock.java
@@ -31,10 +31,11 @@ import mindustry.world.blocks.*;
 import static mindustry.Vars.*;
 
 public class HexBlock extends Block{
-    public TextureRegion top0;
-    public TextureRegion top1;
-    public TextureRegion top2;
-    public TextureRegion top3;
+	public TextureRegion top;
+	public TextureRegion top0;
+	public TextureRegion top1;
+	public TextureRegion top2;
+	public TextureRegion top3;
 	public boolean showMinimapColor = true;
 
 	@Override
@@ -43,99 +44,106 @@ public class HexBlock extends Block{
 		return build != null && showMinimapColor ? 0 : build.color;
 	}
 
-    public HexBlock(String name){
-        super(name);
-        update = true;
-        configurable = true;
-        saveConfig = true;
-        envEnabled |= Env.space;
-        swapDiagonalPlacement = true;
+	public HexBlock(String name){
+		super(name);
+		update = true;
+		configurable = true;
+		saveConfig = true;
+		envEnabled |= Env.space;
+		swapDiagonalPlacement = true;
 
-        config(Integer.class, (HexBuild tile, Integer value) -> tile.color = value);
-    }
+		config(Integer.class, (HexBuild tile, Integer value) -> tile.color = value);
+	}
 	
 
 	@Override
 	
 	public void load(){
 		super.load();
-	
-		top0 = Core.atlas.find(name + "-top0"); 
-		top1 = Core.atlas.find(name + "-top1"); 
-		top2 = Core.atlas.find(name + "-top2"); 
-		top3 = Core.atlas.find(name + "-top3"); 
+		if (rotate) {
+			top0 = Core.atlas.find(name + "-top0"); 
+			top1 = Core.atlas.find(name + "-top1"); 
+			top2 = Core.atlas.find(name + "-top2"); 
+			top3 = Core.atlas.find(name + "-top3"); 
+		} else {
+            top = Core.atlas.find(name + "-top"); 
         }
+	}
 
-    public class HexBuild extends Building{
-        public int color = 0x000000_00;
-        public float smoothTime = 1f;
+	public class HexBuild extends Building{
+		public int color = 0x000000_00;
+		public float smoothTime = 1f;
 
-        @Override
-        public void control(LAccess type, double p1, double p2, double p3, double p4){
-            if(type == LAccess.color){
-                color = Tmp.c1.fromDouble(p1).rgba();
-            }
+		@Override
+		public void control(LAccess type, double p1, double p2, double p3, double p4){
+			if(type == LAccess.color){
+				color = Tmp.c1.fromDouble(p1).rgba();
+			}
 
-            super.control(type, p1, p2, p3, p4);
-        }
-
-        @Override
-        public double sense(LAccess sensor){
-            if(sensor == LAccess.color) return Tmp.c1.set(color).toDoubleBits();
-            return super.sense(sensor);
-        }
-
-        @Override
-        public void draw(){
-            super.draw();
-            Draw.color(Tmp.c1.set(color));
-		switch(rotation){
-			case 0: Draw.rect(top0, x, y); break;
-			case 1: Draw.rect(top1, x, y); break;
-			case 2: Draw.rect(top2, x, y); break;
-			case 3: Draw.rect(top3, x, y); break;
+			super.control(type, p1, p2, p3, p4);
 		}
-            Draw.color();
-        }
 
-        @Override
-        public void updateTile(){
-            smoothTime = Mathf.lerpDelta(smoothTime, timeScale, 0.1f);
-        }
+		@Override
+		public double sense(LAccess sensor){
+			if(sensor == LAccess.color) return Tmp.c1.set(color).toDoubleBits();
+			return super.sense(sensor);
+		}
 
-        @Override
-        public void buildConfiguration(Table table){
-            table.button(Icon.pencil, Styles.cleari, () -> {
-                ui.picker.show(Tmp.c1.set(color), true, res -> configure(res.rgba()));
-                deselect();
-            }).size(40f);
-        }
-
-        @Override
-        public boolean onConfigureBuildTapped(Building other){
-            if(this == other){
-                deselect();
-                return false;
+		@Override
+		public void draw(){
+			super.draw();
+			Draw.color(Tmp.c1.set(color));
+			if (HexBlock.rotate) {
+				switch(rotation){
+					case 0: Draw.rect(top0, x, y); break;
+					case 1: Draw.rect(top1, x, y); break;
+					case 2: Draw.rect(top2, x, y); break;
+					case 3: Draw.rect(top3, x, y); break;
+				}
+        	} else {
+                Draw.rect(top, x, y);
             }
+			Draw.color();
+		}
 
-            return true;
-        }
+		@Override
+		public void updateTile(){
+			smoothTime = Mathf.lerpDelta(smoothTime, timeScale, 0.1f);
+		}
 
-        @Override
-        public Integer config(){
-            return color;
-        }
+		@Override
+		public void buildConfiguration(Table table){
+			table.button(Icon.pencil, Styles.cleari, () -> {
+				ui.picker.show(Tmp.c1.set(color), true, res -> configure(res.rgba()));
+				deselect();
+			}).size(40f);
+		}
 
-        @Override
-        public void write(Writes write){
-            super.write(write);
-            write.i(color);
-        }
+		@Override
+		public boolean onConfigureBuildTapped(Building other){
+			if(this == other){
+				deselect();
+				return false;
+			}
 
-        @Override
-        public void read(Reads read, byte revision){
-            super.read(read, revision);
-            color = read.i();
-        }
-    }
+			return true;
+		}
+
+		@Override
+		public Integer config(){
+			return color;
+		}
+
+		@Override
+		public void write(Writes write){
+			super.write(write);
+			write.i(color);
+		}
+
+		@Override
+		public void read(Reads read, byte revision){
+			super.read(read, revision);
+			color = read.i();
+		}
+	}
 }
