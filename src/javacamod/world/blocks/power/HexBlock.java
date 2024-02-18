ef35cf17b65bc7793770fdac879893913db1fddd
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2024-02-18 10:50:29 +0100

    I tried fixing the wrong thing

--- src/javacamod/world/blocks/power/HexBlock.java
+++ src/javacamod/world/blocks/power/HexBlock.java
@@ -60,7 +60,7 @@ public class HexBlock extends Block{
 	
 	public void load(){
 		super.load();
-		if (rotatedOutput(0,0)) {
+		if (rotate) {
 			top0 = Core.atlas.find(name + "-top0"); 
 			top1 = Core.atlas.find(name + "-top1"); 
 			top2 = Core.atlas.find(name + "-top2"); 
@@ -93,7 +93,7 @@ public class HexBlock extends Block{
 		public void draw(){
 			super.draw();
 			Draw.color(Tmp.c1.set(color));
-			if (HexBlock.rotate) {
+			if (HexBlock.rotatedOutput()) {
 				switch(rotation){
 					case 0: Draw.rect(top0, x, y); break;
 					case 1: Draw.rect(top1, x, y); break;
