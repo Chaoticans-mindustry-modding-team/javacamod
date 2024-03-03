9d564526cd41e483582f71801c5affb0705a0d79
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-03-03 16:52:19 -0700

    Update JavacamodBlocksOverride.java

--- src/javacamod/content/JavacamodBlocksOverride.java
+++ src/javacamod/content/JavacamodBlocksOverride.java
@@ -1,3 +1,43 @@
+package javacamod.content;
+
+import mindustry.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.DrawPart.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.unit.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.heat.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+import javacamod.world.blocks.logic.*;
+import javacamod.world.blocks.decoration.*;
+import javacamod.world.blocks.distribution.*;
+
+import static mindustry.Vars.*;
+
 public class JavacamodBlocksOverride{
   public static void init(){
     Blocks.message.maxTextLength = 22000;
