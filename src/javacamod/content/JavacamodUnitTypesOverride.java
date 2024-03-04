dec1469fee49ef2c79a5d19295b79ce19617b041
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-03-03 18:04:06 -0700

    Update JavacamodUnitTypesOverride.java

--- src/javacamod/content/JavacamodUnitTypesOverride.java
+++ src/javacamod/content/JavacamodUnitTypesOverride.java
@@ -46,7 +46,7 @@ import static mindustry.Vars.*;
 public class JavacamodUnitTypesOverride{
   public static void init(){
 
-    ((Units)UnitTypes.dagger).canBuild = true;
+    ((UnitType)UnitTypes.dagger).canBuild = true;
     UnitTypes.dagger.buildSpeed = 1;
 
   }
