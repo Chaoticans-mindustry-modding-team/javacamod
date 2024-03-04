5da02d4e7f9c38dd2f5a45aa0f3148cd68d9c72f
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-03-03 18:21:58 -0700

    Update JavacamodUnitTypesOverride.java

--- src/javacamod/content/JavacamodUnitTypesOverride.java
+++ src/javacamod/content/JavacamodUnitTypesOverride.java
@@ -47,6 +47,59 @@ public class JavacamodUnitTypesOverride{
   public static void init(){
 
     UnitTypes.dagger.buildSpeed = 1;
+    UnitTypes.mace.buildSpeed = 1;
+    UnitTypes.fortress.buildSpeed = 1;
+    UnitTypes.scepter.buildSpeed = 1;
+    UnitTypes.reign.buildSpeed = 1;
+    UnitTypes.crawler.buildSpeed = 1;
+    UnitTypes.atrax.buildSpeed = 1;
+    UnitTypes.spiroct.buildSpeed = 1;
+    UnitTypes.arkyid.buildSpeed = 1;
+    UnitTypes.toxopid.buildSpeed = 1;
+    UnitTypes.nova.buildSpeed = 1;
+    UnitTypes.pulsar.buildSpeed = 1;
+    UnitTypes.quasar.buildSpeed = 1;
+    UnitTypes.vela.buildSpeed = 1;
+    UnitTypes.corvus.buildSpeed = 1;
+    UnitTypes.flare.buildSpeed = 1;
+    UnitTypes.horizon.buildSpeed = 1;
+    UnitTypes.zenith.buildSpeed = 1;
+    UnitTypes.antumbra.buildSpeed = 1;
+    UnitTypes.eclipse.buildSpeed = 1;
+    UnitTypes.mono.buildSpeed = 1;
+    UnitTypes.poly.buildSpeed = 1;
+    UnitTypes.mega.buildSpeed = 1;
+    UnitTypes.quad.buildSpeed = 1;
+    UnitTypes.oct.buildSpeed = 1;
+    UnitTypes.risso.buildSpeed = 1;
+    UnitTypes.minke.buildSpeed = 1;
+    UnitTypes.bryde.buildSpeed = 1;
+    UnitTypes.sei.buildSpeed = 1;
+    UnitTypes.omura.buildSpeed = 1;
+    UnitTypes.retusa.buildSpeed = 1;
+    UnitTypes.oxynoe.buildSpeed = 1;
+    UnitTypes.cyerce.buildSpeed = 1;
+    UnitTypes.aegires.buildSpeed = 1;
+    UnitTypes.navanax.buildSpeed = 1;
+    UnitTypes.stell.buildSpeed = 1;
+    UnitTypes.locus.buildSpeed = 1;
+    UnitTypes.precept.buildSpeed = 1;
+    UnitTypes.vanquish.buildSpeed = 1;
+    UnitTypes.conquer.buildSpeed = 1;
+    UnitTypes.merui.buildSpeed = 1;
+    UnitTypes.cleroi.buildSpeed = 1;
+    UnitTypes.anthicus.buildSpeed = 1;
+    UnitTypes.tecta.buildSpeed = 1;
+    UnitTypes.collaris.buildSpeed = 1;
+    UnitTypes.elude.buildSpeed = 1;
+    UnitTypes.avert.buildSpeed = 1;
+    UnitTypes.obviate.buildSpeed = 1;
+    UnitTypes.quell.buildSpeed = 1;
+    UnitTypes.disrupt.buildSpeed = 1;
+    UnitTypes.renale.buildSpeed = 1;
+    UnitTypes.renale.hidden = false;
+    UnitTypes.latum.buildSpeed = 1;
+    UnitTypes.latum.hidden = false;
 
   }
 }
