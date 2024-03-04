4d2f87249c9b5f9e0dc7607e982a631358b10cc4
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2024-03-03 17:50:24 -0700

    Update JavacamodBlocksOverride.java

--- src/javacamod/content/JavacamodBlocksOverride.java
+++ src/javacamod/content/JavacamodBlocksOverride.java
@@ -45,6 +45,7 @@ import static mindustry.Vars.*;
 
 public class JavacamodBlocksOverride{
   public static void init(){
+    Blocks.message.maxTextLength = 22000;
     Blocks.reinforcedMessage.maxTextLength = 22000;
   }
 }
