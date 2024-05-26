62facc9e81365d144db51f626fd8c3174d914838
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2024-05-26 06:40:51 -0500

    deforesting

--- src/javacamod/world/blocks/testing/SizeBlock.java
+++ src/javacamod/world/blocks/testing/SizeBlock.java
@@ -36,6 +36,7 @@ public class SizeBlock extends Block{
     public void flipRotation(BuildPlan req, boolean zPressed){
 	Log.info(this);
 	Log.info(this.size);
+	Log.info(size);
 	Log.info(zPressed);
         if (zPressed) {
             this.size = ((this.size + 14) & 15) + 1;
