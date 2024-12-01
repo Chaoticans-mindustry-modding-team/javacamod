5ec987d6be555649e71f498b57fe666f8a2fd8f6
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2024-12-01 06:19:34 -0500

    notinvis?

--- src/javacamod/world/blocks/decoration/HexBlock.java
+++ src/javacamod/world/blocks/decoration/HexBlock.java
@@ -63,7 +63,7 @@ public class HexBlock extends Block{
 		envEnabled |= Env.space;
 		swapDiagonalPlacement = true;
 
-		fullIcon = top1;
+		fullIcon = Core.atlas.find(name + "-top1");
 
 		config(Integer.class, (HexBuild tile, Integer value) -> tile.color = value);
 	}
