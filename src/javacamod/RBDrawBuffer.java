07a4fe8a22bdc462fb14f64115e079a00971d338
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-03-25 10:08:50 -0500

    Update RBDrawBuffer.java

--- src/javacamod/RBDrawBuffer.java
+++ src/javacamod/RBDrawBuffer.java
@@ -4,6 +4,7 @@ public class RBDrawBuffer {
   
   public String[] drawType;
   public Object[][] drawArgs;
+  public int index = 0;
   
   public RBDrawBuffer(int limit) {
     drawType = new String[limit];
