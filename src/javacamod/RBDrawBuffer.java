a3013c5969e36d231768d6d4bd2170058c62befa
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-03-26 12:38:58 -0500

    Update RBDrawBuffer.java

--- src/javacamod/RBDrawBuffer.java
+++ src/javacamod/RBDrawBuffer.java
@@ -5,9 +5,18 @@ public class RBDrawBuffer {
   public String[] drawType;
   public Object[][] drawArgs;
   public int currentSize = 0;
+  public int sizeLimit
   
   public RBDrawBuffer(int limit) {
+    sizeLimit = limit;
     drawType = new String[limit];
     drawArgs = new Object[limit][];
+  }
+
+  public void append(String type, Object[] args) {
+    if (currentSize >= sizeLimit) return;
+    drawType[currentSize] = type;
+    drawArgs[currentSize] = args;
+    currentSize++;
   }
 }
