cff535d3f2c0381c6714464c7fa5b4733db9fcd3
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-03-31 13:23:48 -0500

    compensate for bad centering

--- src/javacamod/world/blocks/logic/RenderBlock.java
+++ src/javacamod/world/blocks/logic/RenderBlock.java
@@ -117,7 +117,7 @@ public class RenderBlock extends Block {
 						}
 						break;
 					case "print":
-						if (args[0] instanceof String t && args[1] instanceof Vec2 p && args[2] instanceof Float s) WorldLabel.drawAt(t, x + p.x, y + p.y, Layer.block, 0, s);
+						if (args[0] instanceof String t && args[1] instanceof Vec2 p && args[2] instanceof Float s) WorldLabel.drawAt(t, x + p.x, y + p.y + 2*s, Layer.block, 0, s);
 						break;
 					case "setregion":
 						if (args[0] instanceof String c) {
