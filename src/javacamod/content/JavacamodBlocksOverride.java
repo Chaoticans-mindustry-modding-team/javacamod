408d8ff938e3771a75ee9794a2fd97d393c29bdf
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-04-21 15:52:30 -0500

    missed one

--- src/javacamod/content/JavacamodBlocksOverride.java
+++ src/javacamod/content/JavacamodBlocksOverride.java
@@ -2,8 +2,6 @@ package javacamod.content;
 
 import mindustry.world.blocks.logic.*;
 
-import static mindustry.Vars.*;
-
 public class JavacamodBlocksOverride{
   public static void init(){
     ((MessageBlock)Blocks.message).maxTextLength = 22000;
