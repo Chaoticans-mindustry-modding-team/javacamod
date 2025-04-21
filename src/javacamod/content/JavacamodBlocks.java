83bc4df3462d74a1da75097b4c4f546d2321d86d
The4codeblocks <72419529+The4codeblocks@users.noreply.github.com>
2025-04-21 16:00:01 -0500

    Update JavacamodBlocks.java

--- src/javacamod/content/JavacamodBlocks.java
+++ src/javacamod/content/JavacamodBlocks.java
@@ -6,6 +6,7 @@ import mindustry.world.blocks.defense.*;
 import mindustry.world.blocks.distribution.*;
 import mindustry.world.blocks.liquid.*;
 import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.power.*;
 import mindustry.world.blocks.sandbox.*;
 
 import javacamod.world.blocks.logic.*;
