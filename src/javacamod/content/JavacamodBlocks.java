75cf457b6275632a9bcb8cbd3d993b0208a7116c
Xaocsoul <128741371+Xaocsoul@users.noreply.github.com>
2026-08-07 03:45:03 -0600

    Update JavacamodBlocks.java

--- src/javacamod/content/JavacamodBlocks.java
+++ src/javacamod/content/JavacamodBlocks.java
@@ -101,7 +101,7 @@ textureBlock, textureImitator, textureGrid, textureBlockShadowless, textureImita
 lightSourceTiny, lightSourceSmall, lightSource, lightSourceLarge, lightSourceHuge,
 
 //Decor
-darkerUnloader, vial, hexVial, chain, chainTurn, chainT, chainX, chainD, chainDl, chainDt, chainDx, deChain, deChainL, deChainT, deChainX, deChainD, deChainDl, deChainDt, deChainDx, jointOne, jointTwo, jointThree, jointFour, jointFive, jointSix, jointSeven, jointAOne, jointATwo, jointAThree, jointAFour, jointAFive, jointSOne, jointSTwo, jointSThree, jointSFour,
+darkerUnloader, vial, hexVial, chain, chainTurn, chainT, chainX, chainD, chainDl, chainDt, chainDx, deChain, deChainL, deChainT, deChainX, deChainD, deChainDl, deChainDt, deChainDx, jointOne, jointTwo, jointThree, jointFour, jointFive, jointSix, jointSeven, jointAOne, jointATwo, jointAThree, jointAFour, jointAFive, jointSOne, jointSTwo, jointSThree, jointSFour
 
 //Render block
 //renderBlock
